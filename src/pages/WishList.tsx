c654e2c9c542fdc930845799e240338501c67638
codeCampWithSifat <sayedhossainsifat300@.com>
2023-07-29 03:34:38 +0600

    wishlist part done

--- src/pages/WishList.tsx
+++ src/pages/WishList.tsx
@@ -5,14 +5,16 @@ const WishList = () => {
   console.log(books);
   return (
     <div>
-      <h2>Set For WishList</h2>
+      <h2 className="text-center text-indigo-700 font-bold text-3xl">
+        Set For WishList
+      </h2>
       <div className="grid lg:grid-cols-3 md: grid-cols-2 sm:grid-cols-1 gap-5">
         {books.map((book) => (
           <div className="card  bg-base-100 shadow-xl">
             <div className="card-body">
               <h2 className="card-title">Author : {book.author}</h2>
               <p>Genre : {book.genre}</p>
-              <p>Genre : {book.publicationDate}</p>
+              <p>Publication Date : {book.publicationDate}</p>
               <p>Review : {book.review}</p>
               <p>Title : {book.title}</p>
             </div>
